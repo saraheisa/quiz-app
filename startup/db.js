5a6462f08d02026d7233b8ba626663baead58d8c
Sara Eisa <prosarahgamal@outlook.com>
2019-07-25 17:55:37 +0200

    connect to db

---
+++ startup/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const winston=require('winston');
+const config=require('config');
+
+module.exports = function () {
+    const db=config.get('db');
+    mongoose.connect(db, {
+            useNewUrlParser: true
+        })
+        .then(() => winston.info(`Connected to ${db} `));
+}
