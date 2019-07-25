6a54601d0875831c6946d04fb1902e4b91ee7159
Sara Eisa <prosarahgamal@outlook.com>
2019-07-25 16:05:39 +0200

    add question model

---
+++ models/question.js
@@ -0,0 +1,38 @@
+const joi = require('joi');
+const mongoose = require('mongoose');
+
+const queSchema = new mongoose.Schema({
+    que: {
+        type: String,
+        required: true,
+        maxlength: 255
+    },
+    answers: {
+        type: Array,
+        required: true,
+        minlength: 2
+    },
+    correct: {
+        type: String,
+        required: true
+    },
+    explanation: {
+        type: String,
+        required: true
+    }
+});
+
+const Question = mongoose.model('Question', queSchema);
+
+function validateQue(que) {
+    const schema = {
+        que: joi.string().max(255).required(),
+        answers: joi.array().min(2).required(),
+        correct: joi.string().required(),
+        explanation: joi.string().required()
+    };
+    return joi.validate(que, schema);
+}
+
+exports.question = Question;
+exports.validate = validateQue;
