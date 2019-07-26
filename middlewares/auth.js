46425f1cc29ce2fd969216f9283a59b5dbe2e43b
Sara Eisa <prosarahgamal@outlook.com>
2019-07-26 04:31:22 +0200

    redirect to login if there's no token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,14 @@ const jwt=require('jsonwebtoken');
 const config=require('config');
 
 module.exports=function (req,res,next) {
-    const token=req.header('x-auth-token');
-    if(!token) return res.status(401).send('Access Denied: Invalid Token');
+    const token=req.header.token;
+    if(!token) return res.redirect('/login');
 
     try {
         const decode=jwt.verify(token,config.get('jwtPrivateKey'));
         req.user=decode;
         next();
     } catch (ex) {
-        res.status(400).send('Invalid Token');
+        res.redirect('/login');
     }
 }
